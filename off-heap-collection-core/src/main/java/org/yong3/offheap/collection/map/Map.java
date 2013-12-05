88d56dc8b6399b172a4b8fe1e684b0d7879c5b16
YC Huang <ychuang.cn@gmail.com>
2013-12-05 16:25:57 +0800

    add Map interface

---
+++ off-heap-collection-core/src/main/java/org/yong3/offheap/collection/map/Map.java
@@ -0,0 +1,12 @@
+package org.yong3.offheap.collection.map;
+
+public interface Map<K, V> {
+
+	public void put(K key, V value);
+	
+	public V get(K key);
+	
+	public V remove(K key);
+	
+	public void destroy();
+}
