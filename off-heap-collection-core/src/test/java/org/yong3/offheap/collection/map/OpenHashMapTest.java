bd1dea11eec793af332632231030fbd07f5bb37e
YC Huang <ychuang.cn@gmail.com>
2013-12-18 17:27:13 +0800

    add NestedOpenHashMap, which has small max_probe setting, and use another map to store keys that exceed max_probe, might performs well than OpenHashMap

--- off-heap-collection-core/src/test/java/org/yong3/offheap/collection/map/OpenHashMapTest.java
+++ off-heap-collection-core/src/test/java/org/yong3/offheap/collection/map/OpenHashMapTest.java
@@ -26,63 +26,129 @@ public class OpenHashMapTest {
 	}
 
 	@Test
-	public void testRawStringKey() {
-		MemoryAllocator allocator = MemoryAllocatorFactory.get();
-		Serializer<String> keySerializer = SerializerFactory.get(String.class);
-		Serializer<String> valueSerializer = SerializerFactory
-				.get(String.class);
-		assertTrue(valueSerializer.getClass() == StringSerializer.class);
-
-		int MAP_SIZE = 1 * 1024 * 1024;
-		OpenHashMap<String, String> map = new OpenHashMap<String, String>(
+	public void testRawStringKeyPerf() {
+		int MAP_SIZE = 10 * 1024 * 1024;		
+		
+		//warmup
+		this.testRawStringKey();
+		
+		NestedOpenHashMap<String, String> map = new NestedOpenHashMap<String, String>(
 				String.class, String.class, MAP_SIZE);
 
+		
+		long start = System.nanoTime();
 		for (int i = 0; i < MAP_SIZE; i++) {
-			assertFalse(map.contains(new String("" + i)));
+			map.contains(new String("" + i));
 		}
+		System.err.printf("existence check cost:%d ns/op\n", (System.nanoTime() - start)/MAP_SIZE);
 
+		start = System.nanoTime();
 		for (int i = 0; i < MAP_SIZE; i++) {
 			String key = "" + i;
-			long addr = allocator.allocate(keySerializer.getOffheapSize(key));
-			keySerializer.write(addr, key);
-			assertEquals(key, keySerializer.read(addr, String.class));
-			allocator.deallocate(addr);
-
-			addr = allocator.allocate(valueSerializer.getOffheapSize(key));
-			valueSerializer.write(addr, key);
-			assertEquals(key, valueSerializer.read(addr, String.class));
-			allocator.deallocate(addr);
-
-			// assertTrue((key.shortHash() & (byte)0x80) != 0);
 			map.put(key, key);
-			try {
-				assertTrue(map.contains(key));
-				assertEquals(key, map.get(key));
-			} catch (AssertionError e) {
-				System.err.println(i);
-				throw e;
-			}
 		}
 
+		System.err.printf("put cost:%d ns/op\n", (System.nanoTime() - start)/MAP_SIZE);
+
+		start = System.nanoTime();
+		
 		for (int i = 0; i < MAP_SIZE; i++) {
 			String key = "" + i;
-			assertTrue(map.contains(key));
-			assertEquals(key, map.get(key));
+			map.contains(key);
 		}
+		System.err.printf("existence check cost:%d ns/op\n", (System.nanoTime() - start)/MAP_SIZE);
+
+		start = System.nanoTime();
+		
+		for (int i = 0; i < MAP_SIZE; i++) {
+			String key = "" + i;
+			map.get(key);
+		}
+		
+		System.err.printf("get cost:%d ns/op\n", (System.nanoTime() - start)/MAP_SIZE);
+
+		start = System.nanoTime();
 
 		for (int i = 0; i < MAP_SIZE; i++) {
 			String k = "" + i;
 			String v = "" + (i + 1);
 			map.put(k, v);
 		}
+		
+		System.err.printf("update cost:%d ns/op\n", (System.nanoTime() - start)/MAP_SIZE);
+
+		start = System.nanoTime();
+
+
+		for (int i = 0; i < MAP_SIZE; i++) {
+			String k = "" + i;
+			String v = "" + (i + 1);
+			map.contains(k);
+		}
+		System.err.printf("existence check cost:%d ns/op\n", (System.nanoTime() - start)/MAP_SIZE);
+
+		start = System.nanoTime();
+		
+		for (int i = 0; i < MAP_SIZE; i++) {
+			String k = "" + i;
+			String v = "" + (i + 1);
+			map.get(k);
+		}
+		System.err.printf("get cost:%d ns/op\n", (System.nanoTime() - start)/MAP_SIZE);
+
+		start = System.nanoTime();
+		map.destroy();
+		System.err.printf("destroy cost:%d us\n", (System.nanoTime() - start)/1000);
+	}
+
+	
+	@Test
+	public void testRawStringKey() {
+		int MAP_SIZE = 1 * 1024 * 1024;		
+		
+		NestedOpenHashMap<String, String> map = new NestedOpenHashMap<String, String>(
+				String.class, String.class, MAP_SIZE);
+
+	
+		for (int i = 0; i < MAP_SIZE; i++) {
+			assertFalse(map.contains(new String("" + i)));
+		}
+	
+		for (int i = 0; i < MAP_SIZE; i++) {
+			String key = "" + i;
+			map.put(key, key);
+		}
+		
+		for (int i = 0; i < MAP_SIZE; i++) {
+			String key = "" + i;
+			assertTrue(map.contains(key));
+		}
+		
+		for (int i = 0; i < MAP_SIZE; i++) {
+			String key = "" + i;
+			assertEquals(key, map.get(key));
+		}
+		
+
+		for (int i = 0; i < MAP_SIZE; i++) {
+			String k = "" + i;
+			String v = "" + (i + 1);
+			map.put(k, v);
+		}
+		
+
 
 		for (int i = 0; i < MAP_SIZE; i++) {
 			String k = "" + i;
 			String v = "" + (i + 1);
 			assertTrue(map.contains(k));
+		}
+		
+		for (int i = 0; i < MAP_SIZE; i++) {
+			String k = "" + i;
+			String v = "" + (i + 1);
 			assertEquals(v, map.get(k));
 		}
-
 		map.destroy();
 	}
 
